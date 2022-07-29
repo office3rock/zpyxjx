57357b73de072085075d7fc8500793b5db2d6f55
boazglick <95533522+boazglick@users.noreply.github.com>
2022-07-29 17:58:22 +0300

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -3,14 +3,14 @@ module.exports = {
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-  siteTitleMeta: "קידום אתרים אלנקלר", // This allows an alternative site title for meta data for pages.
+  siteTitleMeta: "alankaler", // This allows an alternative site title for meta data for pages.
   siteDescriptionMeta:
-    "קידום אתרים ובניית קישורים", // This allows an alternative site description for meta data for pages.
+    "alankaler", // This allows an alternative site description for meta data for pages.
 
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
 
-  shortTitle: "קידום אתרים אלנקלר", // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: "alankaler", // Used for App manifest e.g. Mobile Home Screen
   siteIcon: "favicon.png", // Logo in /static dir used for SEO, RSS, and App manifest
   backgroundColor: "#e9e9e9", // Used for Offline Manifest
   themeColor: "#15171A", // Used for Offline Manifest
