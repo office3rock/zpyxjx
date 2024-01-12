deb37bc6c5311b65c3b2573d7e0b1399c2126c00
office3rock <105702924+office3rock@users.noreply.github.com>
2024-01-12 14:31:51 +0200

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -18,26 +18,26 @@ module.exports = {
   subscribeWidget: {
     visible: false,
     title: "zpyxjx.com",
-    helpText: "הישארו מעודכנים",
-    successMessage: "תודה ההודעה נשלחה בהצלחה",
+    helpText: "Keep following",
+    successMessage: "Thank you The message was sent successfully",
   },
   header: {
     navigation: [
       {
-        label: "דף הבית",
+        label: "Homepage",
         url: "https://zpyxjx.com/",
       },
       {
-        label: "יצירת קשר",
+        label: "Contact",
         url: "https://zpyxjx.com/contact",
       },
     ],
   },
   footer: {
-    copyright: "כל הזכויות שמורות ",
+    copyright: "All rights reserved ",
     navigation: [
       {
-        label: "דף הבית",
+        label: "Homepage",
         url: "https://zpyxjx.com/",
       },
       {
@@ -46,7 +46,7 @@ module.exports = {
       },
       
       {
-        label: "יצירת קשר",
+        label: "Contact",
         url: "https://zpyxjx.com/contact",
       },
      
